957802ad8dcad416fe2bdc2e35739f09d5c59af3
danielmckinnonanderson <danielmckinnon.anderson@gmail.com>
2025-03-28 16:15:38 -0500

    WIP

--- src/fen.zig
+++ src/fen.zig
@@ -1,69 +1,78 @@
-const std = @import("std");
-const bitboards = @import("bitboards.zig");
-const Board = bitboards.Board;
-const PieceInfo = bitboards.PieceInfo;
-
+const std        = @import("std");
+const bitboards  = @import("bitboards.zig");
+const Board      = bitboards.Board;
+const PieceInfo  = bitboards.PieceInfo;
+const Piece      = bitboards.Piece;
+const PieceColor = bitboards.PieceColor;
 
 fn parseFenPositions(fen_string: []const u8) !Board {
-    const PosParserState = union(enum) {
-        initial,
-        parsing_row: enum {
-            start,
-            empty,
-            piece,
-            delim,
-        },
-        finished,
-    };
-
     var result = Board.init();
-    var state = PosParserState.initial;
 
     // FEN strings are declared from left (A) to right (H)
     // starting at the back (A8 [56] -> H8 [63]) and then moving down a
-    // row (A7 -> H7) until the final row (A1 [0] -> H1 [7]) is parsed
-    var pos_idx = 56;
+    // row (A7 -> H7) until the final row (A1 [0] -> H1 [7]) is parsed.
+    //
+    // We will mutate this variable as we insert pieces on 
+    var pos_idx: Board.PositionIndex = 56;
 
-    var char_idx = 0;
+    const Pair = packed struct {
+        pos:   Board.PositionIndex,
+        piece: Piece,
+        col:   PieceColor,
+    };
 
-    while (state != .finished) : (char_idx != fen_string.len) {
-        switch (state) {
-            .initial => {
-                // Nothing to set up, just begin parsing row
-                state = .{ .parsing_row = .start };
+    var positions: [64]Pair = undefined;
+
+    for (0.., fen_string) |char_idx, char| {
+        _ = char_idx;
+
+        switch (char) {
+            '0'...'9' => |n| {
+                std.fmt.parseInt(u8, n, 10) catch unreachable;
             },
-            .parsing_row => |row_state| {
-                const char = fen_string[char_idx];
-
-                if ((pos_idx + 1) % 8 == 0) {
-                    // Expect a delimiter here
-                }
-
-                switch (row_state) {
-                    // Start of row could be either a piece or an empty indicator
-                    .start => {
-                        // Check to see if it is an integer indicating blanks
-                        if (std.fmt.parseInt(u8, char, 10)) |int| {
-                            // Int is number of empty spaces,
-                            // advance index by that amount
-                            char_idx += int;
-                            state = .{ .parsing_row = .empty };
-                            continue;
-
-                        } else {
-                            // Not an int, we expect a piece.
-                            // An error here would be illegal.
-                            const piece: PieceInfo = try PieceInfo.fromChar(char);
-                            result.setPieceAt(
-                                piece.piece,
-                                piece.color,
-                                bitboards.Position.intoIndex(char_idx));
-                            char_idx += 1;
-                        }
-                    }
-                }
-            }
+            '/' => |delim| {
+                _ = delim;
+            },
+            'b' => {},
         }
+
+        // switch (state) {
+        //     .initial => {
+        //         // Nothing to set up, just begin parsing row
+        //         state = .{ .parsing_row = .start };
+        //     },
+        //     .parsing_row => |row_state| {
+        //         const char = fen_string[char_idx];
+
+        //         if ((pos_idx + 1) % 8 == 0) {
+        //             // Expect a delimiter here
+        //         }
+
+        //         switch (row_state) {
+        //             // Start of row could be either a piece or an empty indicator
+        //             .start => {
+        //                 // Check to see if it is an integer indicating blanks
+        //                 if (std.fmt.parseInt(u8, char, 10)) |int| {
+        //                     // Int is number of empty spaces,
+        //                     // advance index by that amount
+        //                     char_idx += int;
+        //                     state = .{ .parsing_row = .empty };
+        //                     continue;
+
+        //                 } else {
+        //                     // Not an int, we expect a piece.
+        //                     // An error here would be illegal.
+        //                     const piece: PieceInfo = try PieceInfo.fromChar(char);
+        //                     result.setPieceAt(
+        //                         piece.piece,
+        //                         piece.color,
+        //                         bitboards.Position.intoIndex(char_idx));
+        //                     char_idx += 1;
+        //                 }
+        //             }
+        //         }
+        //     }
+        // }
     }
 
     // for (0..fen_string.len) |i| {
